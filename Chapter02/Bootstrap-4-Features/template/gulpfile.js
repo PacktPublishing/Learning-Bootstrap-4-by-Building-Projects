03f86ac307f874098d1448db1853440b74f2e57b
packtleena <leenapatil@packtpub.com>
2018-08-30 20:09:10 +0530

    Add files via upload

---
+++ Chapter02/Bootstrap-4-Features/template/gulpfile.js
@@ -0,0 +1,34 @@
+var gulp = require('gulp')
+var browserSync = require('browser-sync').create()
+var sass = require('gulp-sass')
+
+gulp.task('compile-sass', function() {
+    return gulp.src(['node_modules/bootstrap/scss/bootstrap.scss', 'src/scss/*.scss'])
+    .pipe(sass())
+    .pipe(gulp.dest('src/css'))
+    .pipe(browserSync.stream())
+})
+
+gulp.task('move-js', function() {
+    return gulp.src(['node_modules/bootstrap/dist/js/bootstrap.min.js', 'node_modules/jquery/dist/jquery/jquery.min.js'])
+    .pipe(gulp.dest('src/js'))
+    .pipe(browserSync.stream())
+})
+
+//run sass when serve runs
+//run server
+//watch for any changes in src/scss folder and reload the browser
+//also watch for sass changes
+//watch for html changes
+gulp.task('launch-server', ['compile-sass'], function() {
+    browserSync.init({
+        server: "./src"
+    })
+    gulp.watch(['node_modules/bootstrap/scss/bootstrap.scss', 'src/scss/*.scss'],['compile-sass'])
+    gulp.watch("src/*.html").on('change', browserSync.reload)
+})
+
+//run gulp
+//launch server and browser
+//execute js task
+gulp.task('default', ['move-js', 'launch-server'])
